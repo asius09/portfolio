5fdf3d77edd73db40458d22384113dbde3da13a0
Bobby <itsmeasius@gmail.com>
2026-09-05 23:03:34 +0530

    chore: update BCA education to final year

--- src/data/education.ts
+++ src/data/education.ts
@@ -2,7 +2,7 @@ export const education: { institution: string, qualification: string, details: s
   {
     institution: "Indira Gandhi National Open University (IGNOU)",
     qualification: "Bachelor of Computer Applications (BCA)",
-    details: "3rd Year (Pursuing)",
+    details: "Final Year (Pursuing)",
   },
   {
     institution: "Shri Guru Tegh Bahadur Khalsa Boys Sr. Sec. School",
